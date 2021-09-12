473267d5f712abcd9d5812868ff26d1ae195f4f9
Brian Luo <existence8@gmail.com>
2021-09-12 10:49:50 +0700

    save to file

--- products.py
+++ products.py
@@ -6,10 +6,18 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入價格：')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
 #這邊印出每個p裡的第一格，也就是商品名稱
+
+#62寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+
 
